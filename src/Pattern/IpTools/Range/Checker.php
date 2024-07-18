1672157400e2b4481a726784d1a2d3e6fbf2a1e4
Xorg <xorg@neonatis.com>
2024-07-18 17:07:47 +0200

    add to config yaml : range ip local exclude  $ip      = '192.168.0.22';  $checker = Checker::forIp($ip);    $checker->setRange([       '192.168.0.1',       '192.168.0.28'   ]);    $checker->setRange('192.168.0.*');   $checker->setRange('192.168.0.4-192.168.1.54');

--- src/Pattern/IpTools/Range/Checker.php
+++ src/Pattern/IpTools/Range/Checker.php
@@ -33,7 +33,6 @@
      *  192.168.0.41 will return TRUE
      *  192.168.0.61 will return FALSE
      */
-
     class Checker
     {
         protected $ipAddress;
@@ -57,26 +56,53 @@
 
         public function setRange($range)
         {
+            $createRange = function ($startStr, $endStr = null) {
+                $start = IPAddress::fromIPString(trim($startStr));
+                $end   = $endStr ? IPAddress::fromIPString(trim($endStr)) : $start;
+                return Range::fromIPs($start, $end);
+            };
+
+            $handleWildcard = function ($range) use ($createRange) {
+                $startStr = str_replace('*', '0', $range);
+                $endStr   = str_replace('*', '255', $range);
+                return $createRange($startStr, $endStr);
+            };
+
             if (is_array($range)) {
-                $start = IPAddress::fromIPString($range[ 0 ]);
-                $end   = IPAddress::fromIPString($range[ 1 ]);
-            } else if (strpos($range, '-')) {
-                $ranges = explode('-', $range);
-                $start  = IPAddress::fromIPString($ranges[ 0 ]);
-                $end    = IPAddress::fromIPString($ranges[ 1 ]);
+                $this->range = $createRange($range[ 0 ], $range[ 1 ]);
+            } elseif (strpos($range, '-')) {
+                list($startStr, $endStr) = explode('-', $range);
+                $this->range = $createRange($startStr, $endStr);
+            } elseif (substr_count($range, '*') > 0) {
+                $this->range = $handleWildcard($range);
             } else {
-                if (substr_count($range, '*') == 2) {
-                    $startFirstRun = str_replace('.*.', '.0.', $range);
-                    $endFirstRun   = str_replace('.*.', '.255.', $range);
-                    $start         = IPAddress::fromIPString(str_replace('*', '0', $startFirstRun));
-                    $end           = IPAddress::fromIPString(str_replace('*', '255', $endFirstRun));
-                } else if (substr_count($range, '*') == 1) {
-                    $start = IPAddress::fromIPString(str_replace('*', '0', $range));
-                    $end   = IPAddress::fromIPString(str_replace('*', '255', $range));
-                }
+                $this->range = $createRange($range);
             }
-            $this->range = Range::fromIPs($start, $end);
         }
+
+
+//        public function setRange($range)
+//        {
+//            if (is_array($range)) {
+//                $start = IPAddress::fromIPString($range[ 0 ]);
+//                $end   = IPAddress::fromIPString($range[ 1 ]);
+//            } else if (strpos($range, '-')) {
+//                $ranges = explode('-', $range);
+//                $start  = IPAddress::fromIPString($ranges[ 0 ]);
+//                $end    = IPAddress::fromIPString($ranges[ 1 ]);
+//            } else {
+//                if (substr_count($range, '*') == 2) {
+//                    $startFirstRun = str_replace('.*.', '.0.', $range);
+//                    $endFirstRun   = str_replace('.*.', '.255.', $range);
+//                    $start         = IPAddress::fromIPString(str_replace('*', '0', $startFirstRun));
+//                    $end           = IPAddress::fromIPString(str_replace('*', '255', $endFirstRun));
+//                } else if (substr_count($range, '*') == 1) {
+//                    $start = IPAddress::fromIPString(str_replace('*', '0', $range));
+//                    $end   = IPAddress::fromIPString(str_replace('*', '255', $range));
+//                }
+//            }
+//            $this->range = Range::fromIPs($start, $end);
+//        }
 
         public function check()
         {
